f5af368ece59edf55e1d07ad07365c913a213cac
GiovannyRoman <groman@hawk.iit.edu>
2016-08-11 09:55:50 -0500

    created networking app

--- src/day4/Client.java
+++ src/day4/Client.java
@@ -1,9 +1,33 @@
 package day4;
 
+//import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try
+		(
+			Socket ser = new Socket("10.1.1.143",12345);
+			OutputStream out = ser.getOutputStream();
+			Writer write = new OutputStreamWriter(out);
+			Writer buffwriter = new BufferedWriter(write);
+			
+		)
+		{
+			buffwriter.write( "My name is Giovanny Roman");
+			buffwriter.flush();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 
 	}
 
--- src/day4/Server.java
+++ src/day4/Server.java
@@ -1,10 +1,28 @@
 package day4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try (ServerSocket ss = new ServerSocket(12345);
+				Socket s =ss.accept();
+				InputStream in = s.getInputStream();
+				InputStreamReader read = new InputStreamReader(in);
+				BufferedReader reader = new BufferedReader(read);
+				)	
+		{
+			System.out.println(reader.readLine());
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
